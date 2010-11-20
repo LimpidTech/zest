2624201b6192eef2550efab18157927f17ad5be0
Brandon R. Stoner <monokrome@monokro.me>
2010-11-19 22:32:39 -0700

    Finished a basic URL routing mechanism.

--- http/urls.js
+++ http/urls.js
@@ -3,18 +3,18 @@ var module_method_separator = ':'
 	/**
 	 * Object used to represent a specific URL.
 	 */
-var URL = function(request_url, module, method)
+var URL = function(request_uri, module, method)
 {
 	module = require(module)
 
-	this.request_url = request_url
+	this.request_uri = request_uri
 	this.method = module[method]
 }
 
 URL.prototype = new Object()
 
 	// Helper function for generating URLs
-this.url = function(request_url, module)
+this.url = function(request_uri, module)
 {
 	var method,
 	    method_index = module.indexOf(module_method_separator)
@@ -25,6 +25,6 @@ this.url = function(request_url, module)
 	method = module.slice(method_index+1)
 	module = module.slice(0, method_index)
 
-	return new URL(request_url, module, method)
+	return new URL(request_uri, module, method)
 }
 
--- index.js
+++ index.js
@@ -1,18 +1,32 @@
+function resolve_url(url)
+{
+	if (typeof url.request_uri == 'string')// && url.request_uri == this.url)
+		return true
+
+	return false
+}
+
 	/**
 	 * This function is what zest uses to route every request to wherever it is
 	 * that it needs to go.
 	 */
 this.router = function(request, response)
 {
-	var url;
+	// TODO: Hierarchal URLs.
+	var resolved_url, resolved_urls = this.url_patterns.filter(resolve_url, request)
 
-	response.writeHead(200, {
-		'Content-Type': 'text/plain',
-	})
+	if (resolved_urls.length > 1)
+		throw new Error('You can only have one URL matching this request.' +
+		                'There were ' + resolved_urls.length)
 
-	for (url in this.url_patterns)
-		console.log(url)
+	if (resolved_urls.length == 1)
+		resolved_url = resolved_urls[0]
 
+	if (typeof resolved_url != 'undefined' && typeof resolved_url.method == 'function')
+		resolved_url.method(request, response)
+
+	// If we didn't resolve a URL, we have a 404.
+	response.writeHead(404, {})
 	response.end()
 }
 
